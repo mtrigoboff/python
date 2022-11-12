a373296cc93ece65a0256b457274ff304bf842c6
mtrigoboff <mtrigoboff@comcast.net>
2022-11-12 13:30:33 -0800

    more pythonic

--- csv_file/csv_file.py
+++ csv_file/csv_file.py
@@ -3,8 +3,14 @@ data = [[1, 2, 3],
 
 csv_file = open('data.csv', 'w')
 for row in data:
+	csv_file.write(''.join([str(item) + ',' for item in row])[:-1] + '\n')
+
+	'''
 	line = ''
+
 	for item in row:
 		line += str(item) + ','
 	csv_file.write(line[:-1] + '\n')
+	'''
+
 csv_file.close()

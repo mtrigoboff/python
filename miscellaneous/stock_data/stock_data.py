445fe87bf639299a877fb9c9af82cc9c5a8f31e0
Michael Trigoboff <mtrigoboff@comcast.net>
2022-11-13 13:28:55 -0800

    skeleton code for generating csv file from repeated downloads of stock data

--- miscellaneous/stock_data/stock_data.py
+++ miscellaneous/stock_data/stock_data.py
@@ -1,23 +1,58 @@
-import os.path
+import os
+from os.path import join
+from time import sleep
 
-data = {'AAPL' : {'price' : 500, 'shares' : 10000}, 'IBM' : {'price' : 200, 'shares' : 20000}}
+import copy			# only needed for fake data production
 
-csv_file = open(os.path.join('stock_data', 'stock_data.csv'), 'w')
+# fake data for testing purposes
+row_data = {'AAPL' : {'price' : 500, 'shares' : 10000}, 'IBM' : {'price' : 200, 'shares' : 20000}}
 
-# column headers
-line = 'stock,'
-val = list(data.values())[0]
-for key in val.keys():
-	line += key + ','
-csv_file.write(line[:-1] + '\n')
+def get_stock_data(stocks):
 
-# stock data
-for key in data.keys():
-	line = key + ','
-	attrs = data[key]
-	for attr in attrs.keys():
-		line += str(attrs[attr]) + ','
+	# replace with code that actually gets stock data from brokerage
+	
+	# generate different fake data each time
+	row_data['AAPL']['price'] += 1
+	row_data['AAPL']['shares'] += 10
+	row_data['IBM']['price'] += 1
+	row_data['IBM']['shares'] += 10
+
+	return copy.deepcopy(row_data)
+
+def write_csv_file(stock_symbols, file_name, csv_data):
+	
+	csv_file = open(os.path.join('stock_data', file_name), 'w')
+	
+	# column headers
+	line = 'stock,'
+	val = list(csv_data.values())[0][0]
+	for key in val.keys():
+		line += key + ','
 	csv_file.write(line[:-1] + '\n')
 
-csv_file.close()
+	# stock data
+	for stock_symbol in stock_symbols:
+		for stock_data in csv_data[stock_symbol]:
+			line = stock_symbol + ','
+			attrs = stock_data.keys()
+			for attr in attrs:
+				line += str(stock_data[attr]) + ','
+			csv_file.write(line[:-1] + '\n')
+
+	csv_file.close()
+
+def run(interval_secs, file_name):
+	
+	stock_symbols = ['AAPL', 'IBM']				# list of stock symbols
+	csv_data = dict([[stock_symbol, []] for stock_symbol in stock_symbols])
+
+	for rep in range(0, 3):						# loop runs 3 times
+		stock_data = get_stock_data(stock_symbols)
+		for stock_symbol in stock_symbols:
+			csv_data[stock_symbol].append(stock_data[stock_symbol])
+		sleep(interval_secs)					# sleep for designated seconds
+
+	write_csv_file(stock_symbols, file_name, csv_data)
+
+run(1, 'stock_data.csv')
 

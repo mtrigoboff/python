59f80d2edd43739274bde1d17d8e1cf30cbfcf86
mtrigoboff <mtrigoboff@comcast.net>
2022-11-12 16:01:16 -0800

    new project

---
+++ stock_data/stock_data.py
@@ -0,0 +1,21 @@
+data = {'AAPL' : {'price' : 500, 'shares' : 10000}, 'IBM' : {'price' : 200, 'shares' : 20000}}
+
+csv_file = open('data.csv', 'w')
+
+# column headers
+line = 'stock,'
+val = list(data.values())[0]
+for key in val.keys():
+	line += key + ','
+csv_file.write(line[:-1] + '\n')
+
+# stock data
+for key in data.keys():
+	line = key + ','
+	attrs = data[key]
+	for attr in attrs.keys():
+		line += str(attrs[attr]) + ','
+	csv_file.write(line[:-1] + '\n')
+
+csv_file.close()
+

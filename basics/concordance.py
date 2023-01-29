ac56965bbec543d6f1f168091bafb21643ea729d
mtrigoboff <mtrigoboff@comcast.net>
2023-01-28 18:53:53 -0800

    concordance code and text files

---
+++ basics/concordance.py
@@ -0,0 +1,15 @@
+words = {}
+
+text_file = open('constitution.txt', 'r')
+lines = text_file.readlines()
+text_file.close()
+
+for line in lines:
+    for word in line.split():
+        try:
+            words[word] += 1
+        except KeyError:
+            words[word] = 1
+
+for key, value in sorted(words.items()):
+    print(f'{key}: {value}')

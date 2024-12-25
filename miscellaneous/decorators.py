1e1cdd4ed3390a81c901eff3b3557b66813e8f05
mtrigoboff <mtrigoboff@comcast.net>
2024-12-25 13:20:09 -0800

    decorator demo

---
+++ miscellaneous/decorators.py
@@ -0,0 +1,17 @@
+def safe_divide(fn):
+	def dec(num, den):
+		if den == 0:
+			raise ValueError('0 divide!')
+		return fn(num, den)
+	return dec
+
+@safe_divide
+def divide(num, den):
+	return num/den
+
+print(divide(5, 4))
+
+try:
+	print(divide(5, 0))
+except ValueError as e:
+	print(e)
